85b45a81a97862f36610300313a5499fd3504620
DariusAndreiParaschiv <72765284+DariusAndreiParaschiv@users.noreply.github.com>
2022-06-16 20:10:53 +0300

    Build cube and add rotation

---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public GameObject target;
+    public float speed = 50f;
+    bool drag = false;
+    float x = 0;
+    float y = 0;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(1))
+        {
+            drag = false;
+
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            drag = true;
+        }
+        if(Input.GetKeyDown("r"))
+        {
+            this.transform.rotation = Quaternion.Euler(0, 45, 0);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(drag)
+        {
+            x = Input.GetAxis("Mouse X") * speed * Time.fixedDeltaTime;
+            y = Input.GetAxis("Mouse Y") * speed * Time.fixedDeltaTime;
+
+            target.transform.Rotate(y, -x, 0, Space.World);
+        }
+    }
+}
